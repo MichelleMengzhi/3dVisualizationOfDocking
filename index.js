99ebfc6203625ee75f0ab7444e756b0a46af462e
Yuexin Yu <yuexin.yu@mail.utoronto.ca>
2020-02-12 12:14:25 -0500

    label functionadded; not accurate right now; smaller radius of ball and stick

--- index.js
+++ index.js
@@ -44,13 +44,22 @@ request.onload = function () {
 
 }
 
+var v =null;
 
 function find_range(json_file, receptor_name) {
     var result = { max: 0, min: 0 };
     var max = -100000000;
     var min = 0;
+    // for (var i in json_file[receptor_name][ligand_name]) {
+    //     if (max <= json_file[receptor_name][ligand_name][i]) {
+    //         max = json_file[receptor_name][ligand_name][i];
+    //     } else if (min >= json_file[receptor_name][ligand_name][i]) {
+    //         min = json_file[receptor_name][ligand_name][i];
+    //     }
+    // }
     for (var i in json_file[receptor_name]) {
         for (var j in json_file[receptor_name][i]) {
+            //console.log(json_file[receptor_name][i][j]);
             if (max <= json_file[receptor_name][i][j]) {
                 max = json_file[receptor_name][i][j];
             } else if (min >= json_file[receptor_name][i][j]) {
@@ -61,6 +70,7 @@ function find_range(json_file, receptor_name) {
 
     result.max = max;
     result.min = min;
+    //console.log(result.max+" "+result.min);
     return result;
 
 }
@@ -73,16 +83,28 @@ function norm(value, min, max) {
 
 // return the arrary of contact frequency result for the given receptor and ligand pair
 function pos_confreq_array_generator(json_file, receptor_name, ligand_name, range) {
+    //console.log("atom: "+receptor_name.substring(0,4)+" ligand: "+ligand_name)
+    // let normalized_data = {};
+    //console.log(json_file[receptor_name][ligand_name]);
     var max = range.max;
     var min = range.min;
     for (var i in json_file[receptor_name][ligand_name]) {
         json_file[receptor_name][ligand_name][i] = norm(json_file[receptor_name][ligand_name][i], min, max);
     }
+    //console.log(json_file[receptor_name][ligand_name]);
     return json_file[receptor_name][ligand_name];
 }
 
 
-function coloring_function(data, atom) { // data -> array  
+// return the contact frequency of the given atom position from the given position: contact frequency array
+// note that atom_position should be in type of string
+function contfreq_generator(pos_confreq_array, atom_position) {
+    return pos_confreq_array[atom_position];
+}
+
+
+function coloring_function(data, atom) { // data -> array; atom ->   
+    
     let d, d_inv, d_inv_text;
     if (atom.resi in data) {
         d = Math.floor(data[atom.resi] * 255)
@@ -92,6 +114,7 @@ function coloring_function(data, atom) { // data -> array
             d_text = d.toString(16);
         }
         d_inv = 255 - d;
+        //d_inv_text_text = (255-d_text).toString(16);
         if (d_inv.toString(16).length == 1) {
             d_inv_text = "0" + d_inv.toString(16);
         } else {
@@ -104,8 +127,70 @@ function coloring_function(data, atom) { // data -> array
     }
 }
 
+
+var atomcallback = function(atom, viewer) {
+    if (atom.clickLabel === undefined
+            || !atom.clickLabel instanceof $3Dmol.Label) {
+        atom.clickLabel = viewer.addLabel(atom.elem + atom.serial, {
+            fontSize : 14,
+            position : {
+                x : atom.x,
+                y : atom.y,
+                z : atom.z
+            },
+            backgroundColor: "black"
+        });
+        atom.clicked = true;
+    }
+
+    //toggle label style
+    else {
+
+        if (atom.clicked) {
+            var newstyle = atom.clickLabel.getStyle();
+            newstyle.backgroundColor = 0x66ccff;
+
+            viewer.setLabelStyle(atom.clickLabel, newstyle);
+            atom.clicked = !atom.clicked;
+        }
+        else {
+            viewer.removeLabel(atom.clickLabel);
+            delete atom.clickLabel;
+            atom.clicked = false;
+        }
+
+    }
+};
+
+
+
+
 function threeDmol_single_viewer_generator(receptor, data) { // data -> array
-    let receptor_path = './receptors/' + receptor + '.pdb';
+
+    // //$('#viewers').append("<span><div id='" + receptor + "'class='mol-container'></div></span>");
+    // let viewer = $3Dmol.createViewer(receptor);
+    let receptor_path = '../receptors/' + receptor + '.pdb';
+    // jQuery.ajax(receptor_path, {
+    //     success: function (data, pdb) {
+    //         let v = viewer;
+    //         //console.log(data);
+    //         //console.log(receptor);
+    //         //console.log("receptor: " + receptor.substring(0, 4));
+
+    //         v.addModel(pdb, "pdb");                        /* load data */
+    //         v.setStyle({}, { cartoon: { color: 'spectrum' } });  /* style all atoms */
+    //         v.zoomTo();                                      /* set camera */
+    //         v.render();                                      /* render scene */
+    //         v.zoom(1.2, 1000);                               /* slight zoom */
+    //         v.addSurface($3Dmol.SurfaceType.SAS, { // addSurgace(type($3Dmol.SurfaceType), style(SurfaceStyleSpec))
+    //             // opacity:0.9,
+    //             colorfunc: coloring_function.bind(null, data) //SurfaceStyleSpec(opacity:, colorscheme:type(ColorschemeSpec: colorfunc)) 
+    //         });
+    //     }.bind(null, data),
+    //     error: function (hdr, status, err) {
+    //         console.error("Failed to load PDB " + receptor_path + ": " + err);
+    //     },
+    // });
 
     var viewers = $3Dmol.createViewerGrid(
         receptor, //id of div to create canvas in
@@ -114,23 +199,55 @@ function threeDmol_single_viewer_generator(receptor, data) { // data -> array
           cols: 2,
           control_all: true  //mouse controls all viewers
         },
-        { backgroundColor: 'lightgrey' }
+        { backgroundColor: 0xf6f6f6 }
       );
       $.get(receptor_path, function(data, pdb) {
-        var viewer = viewers[0][0];
-        viewer.addModel(pdb,'pdb');
-        viewer.setStyle({}, { cartoon: { color: 'spectrum' } });
-        viewer.zoomTo(1.2, 1000);
-        viewer.render( );
-        viewer.addSurface($3Dmol.SurfaceType.SAS, { // addSurgace(type($3Dmol.SurfaceType), style(SurfaceStyleSpec))
-            // opacity:0.9,
+        v = viewers[0][0];
+        m = v.addModel(pdb,'pdb');
+        atoms = m.selectedAtoms({});
+        for ( var i in atoms) {
+            var atom = atoms[i];
+            atom.clickable = true;
+            atom.callback = atomcallback;
+        }
+        v.setStyle({}, { cartoon: { color: 'spectrum' } });
+        v.mapAtomProperties($3Dmol.applyPartialCharges); //mapAtomPtoperties(props,, sel) 
+                                                        //-> props,: either array of atom selectors 
+                                                        //           with associated props, or function 
+                                                        //           that takes atom and sets its properties
+                                                        //-> sel:  subset of atoms to work on - model 
+                                                        //         selection must be specified here
+        v.zoomTo(1.2, 1000);
+        v.render( );
+        v.addSurface($3Dmol.SurfaceType.SAS, { // addSurgace(type($3Dmol.SurfaceType), style(SurfaceStyleSpec))
+            opacity:0.9,
             colorfunc: coloring_function.bind(null, data) //SurfaceStyleSpec(opacity:, colorscheme:type(ColorschemeSpec: colorfunc)) 
         });
-        viewer = viewers[0][1];
-        viewer.addModel(pdb,'pdb');
-        viewer.setStyle({},{sphere:{radius:0.5}, stick:{radius:0.2}});
-        viewer.zoomTo(1.2, 1000);     
-        viewer.render( );
+        
+
+        v = viewers[0][1];
+        v.addModel(pdb,'pdb');
+        //m = v.addModel(pdb,'pdb');
+        // for ( var i in atoms) {
+        //     var atom = atoms[i];
+        //     atom.clickable = true;
+        //     atom.callback = atomcallback;
+        // }
+        v.setStyle({},{sphere:{radius:0.3}, stick:{radius:0.1}});
+        v.mapAtomProperties($3Dmol.applyPartialCharges); //mapAtomPtoperties(props,, sel) 
+                                                        //-> props,: either array of atom selectors 
+                                                        //           with associated props, or function 
+                                                        //           that takes atom and sets its properties
+                                                        //-> sel:  subset of atoms to work on - model 
+                                                        //         selection must be specified here
+        v.zoomTo(1.2, 1000);     
+        v.render( );
+        v.addSurface($3Dmol.SurfaceType.SAS, { // addSurgace(type($3Dmol.SurfaceType), style(SurfaceStyleSpec))
+            opacity:0.5,
+            colorfunc: coloring_function.bind(null, data) //SurfaceStyleSpec(opacity:, colorscheme:type(ColorschemeSpec: colorfunc)) 
+        });
+
+
         
       }.bind(null, data));
 }
